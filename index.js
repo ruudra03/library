633cc69e375af80f162da3af4d9704d0e0a0ed80
Rudra Patel <work.rudra@aol.com>
2025-03-10 21:44:45 +0000

    Modify index.js to add unique IDs for Book and to add Book to library
    Create library array to store all books.
    Write addBookToLibrary() function which takes required arguments for creating an instance of a new Book. It generates an unique ID for each Book and then adds it to the library.
    Use an example to see the workings.

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 // Log success message
 console.log("JavaScript loaded.");
 
+// Library Object Initialisation
+let library = [];
+
 // Book constructor
-function Book(title, author, pages) {
+function Book(id, title, author, pages) {
   if (!new.target) {
     throw Error("You must use the 'new' operator to call the constructor");
   }
 
+  this.id = id;
   this.title = title; // Name of the book
   this.author = author; // Writer of the book
   this.pages = pages; // Total number of pages in the book
-  this.isRead = false; // Is the book read from the library
+  this.isRead = false; // Is the book read from the library (deafault value is false)
 }
 
 // Get Book information
@@ -21,3 +25,30 @@ Book.prototype.info = function () {
 
   return infoString;
 };
+
+// Set Book is read flag to true
+Book.prototype.isReadTrue = function () {
+  this.isRead = true;
+};
+
+// Add Book to library
+function addBookToLibrary(title, author, pages, isRead) {
+  // Generate unique id
+  const ID = crypto.randomUUID();
+
+  // Create new book using Book constructor
+  let newBook = new Book(ID, title, author, pages);
+
+  // Mark read if required i.e., change default value of false
+  if (isRead) {
+    newBook.isReadTrue();
+  }
+
+  // Add to Library array
+  library.push(newBook);
+
+  return newBook;
+}
+
+// "The Hobbit" example
+console.log(addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295));
