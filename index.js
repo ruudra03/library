643f86e0d908c951a57d891f2fd4502dc6b9e941
Rudra Patel <work.rudra@aol.com>
2025-03-10 20:26:15 +0000

    Modify index.js to add Book constructor and its prototype function
    Write Book() constructor function.
    Throw error if the Book() function is NOT called using 'new' keyword i.e., constructor invocation.
    Add info() prototype to all the Book() objects which returns Book instance's information.

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 // Log success message
 console.log("JavaScript loaded.");
+
+// Book constructor
+function Book(title, author, pages) {
+  if (!new.target) {
+    throw Error("You must use the 'new' operator to call the constructor");
+  }
+
+  this.title = title; // Name of the book
+  this.author = author; // Writer of the book
+  this.pages = pages; // Total number of pages in the book
+  this.isRead = false; // Is the book read from the library
+}
+
+// Get Book information
+Book.prototype.info = function () {
+  let infoString = `${this.title} by ${this.author}, ${this.pages} pages, ${
+    this.isRead ? "read" : "not read yet"
+  }`;
+
+  return infoString;
+};
