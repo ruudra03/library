ddd64975fa812cb6b000ba259ef2f051bbbd96d8
Rudra Patel <work.rudra@aol.com>
2025-03-11 20:49:33 +0000

    Modify index.js to load sample JSON data and render HTML elements
    Add sample books inside data.json using array.
    Insert library container inside the HTML body.
    Render book elements using the data.json when the HTML DOM content is loaded. Use fetch to get JSON data. If success, add books to library, and create and insert respective HTML element in the DOM.
    Write a Book prototype to get HTML string to set inner HTML of the DOM element.
    Allow to pass existing id as an argument when calling addBookToLibrary().
    Modify book CSS class to visualise basic element positioning.

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
-// Log success message
-console.log("JavaScript loaded.");
-
-// Library Object Initialisation
+// Library initialisation
 let library = [];
+
+// Load stored library data
+document.addEventListener("DOMContentLoaded", function () {
+  // Fetch local JSON file
+  fetch("./data.json")
+    .then(function (res) {
+      if (!res.ok) {
+        throw new Error(`HTTP error! ${res.statusText}`);
+      }
+
+      return res.json(); // Parse JSON data
+    })
+    .then(function (data) {
+      // Render book elements from the library
+      renderLibrary(data);
+    })
+    .catch(function (err) {
+      console.error(`Fetch failed! ${err}`);
+    });
+});
 
 // Book constructor
 function Book(id, title, author, pages) {
   if (!new.target) {
-    throw Error("You must use the 'new' operator to call the constructor");
+    throw new Error("You must use the 'new' operator to call the constructor");
   }
 
   this.id = id;
@@ -17,27 +34,67 @@ function Book(id, title, author, pages) {
   this.isRead = false; // Is the book read from the library (deafault value is false)
 }
 
-// Get Book information
-Book.prototype.info = function () {
-  let infoString = `${this.title} by ${this.author}, ${this.pages} pages, ${
-    this.isRead ? "read" : "not read yet"
-  }`;
-
-  return infoString;
-};
-
 // Set Book is read flag to true
 Book.prototype.isReadTrue = function () {
   this.isRead = true;
 };
 
+// Get Book HTML element
+Book.prototype.htmlString = function () {
+  let htmlString = `
+    <span class="book-id">ID: ${this.id}</span>
+    <span class="book-title">${this.title}</span>
+    <span class="book-author">by ${this.author}</span>
+    <span class="book-pages">Pages: ${this.pages}</span>
+    <span class="book-is-read">${this.isRead ? "Read" : "Not Read"}</span>
+  `;
+
+  return htmlString;
+};
+
+// Render books from library to HTML as elements
+function renderLibrary(libraryData) {
+  const container = document.querySelector("#library");
+
+  // Reset container in case of repeated fetch
+  container.innerHTML = "";
+
+  // Create book elements from library
+  libraryData.forEach(function (book) {
+    // Create HTML elements
+    const bookElement = document.createElement("div");
+    bookElement.classList.add("book"); // Add CSS class
+
+    // Add new Book object to library
+    let libraryBook = addBookToLibrary(
+      book.id,
+      book.title,
+      book.author,
+      book.pages,
+      book.isRead
+    );
+
+    bookElement.innerHTML = libraryBook.htmlString();
+
+    // Append book element to library
+    container.appendChild(bookElement);
+  });
+}
+
 // Add Book to library
-function addBookToLibrary(title, author, pages, isRead) {
-  // Generate unique id
-  const ID = crypto.randomUUID();
+function addBookToLibrary(id, title, author, pages, isRead) {
+  let newBookId;
+
+  // Check if ID is provided
+  if (id) {
+    newBookId = id;
+  } else {
+    // Generate unique id
+    newBookId = crypto.randomUUID();
+  }
 
   // Create new book using Book constructor
-  let newBook = new Book(ID, title, author, pages);
+  let newBook = new Book(newBookId, title, author, pages);
 
   // Mark read if required i.e., change default value of false
   if (isRead) {
@@ -49,6 +106,3 @@ function addBookToLibrary(title, author, pages, isRead) {
 
   return newBook;
 }
-
-// "The Hobbit" example
-console.log(addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295));
